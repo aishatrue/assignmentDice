47f00ce01889d5840fdb9086e34ae87e7c66aad5
aishatrue <140340401+aishatrue@users.noreply.github.com>
2024-02-23 16:14:25 +0900

    basic code
    before refactoring code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + "장난이지롱");
+        System.out.print("숫자를 입력하세요: ");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        int r1 = 0,r2 = 0,r3 = 0,r4 = 0,r5 = 0,r6 = 0;
+
+        for(int i=0;i<a;i++){
+            double b = Math.random() * 6;
+            if(b>=0 && b<1){
+                r1++;
+
+            }else if(b>=1 && b<2){
+                r2++;
+            }else if(b>=2 && b<3){
+                r3++;
+            }else if(b>=3 && b<4){
+                r4++;
+            }else if(b>=4 && b<5){
+                r5++;
+            }else if(b>=5 && b<6){
+                r6++;
+            }
         }
-    }
+        System.out.printf("1은 %d번 나왔습니다.\n",r1);
+        System.out.printf("2는 %d번 나왔습니다.\n",r2);
+        System.out.printf("3은 %d번 나왔습니다.\n",r3);
+        System.out.printf("4는 %d번 나왔습니다.\n",r4);
+        System.out.printf("5는 %d번 나왔습니다.\n",r5);
+        System.out.printf("6은 %d번 나왔습니다.\n",r6);
+
+        }
+
 }
